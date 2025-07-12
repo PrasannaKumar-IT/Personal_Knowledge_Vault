1dfd9997be3095a65726afe5a0d0ad87315d9a15
PrasannaKumar-IT <bs8479683@gmail.com>
2025-07-12 14:59:41 +0530

    Feat: add extra features

--- knowledge_vault.py
+++ knowledge_vault.py
@@ -65,8 +65,9 @@ def search_note():
         print(f"Tags: {note[4]}")
         print(f"Created at: {note[5]}")
         print('-'*40)
+
 def filter_notes():
-    print("\Filter Options:")
+    print("\nFilter Options:")
     print("1. Filter by Category")
     print("2. Filter by Tag")
     choice = input("Choose an option (1/2): ").strip()
@@ -77,7 +78,7 @@ def filter_notes():
     elif choice=='2':
         tag=input("Enter a tag for filter: ")
         query="select * from notes where tags like ? order by created_at DESC"
-        params=(f"%{tag}%")
+        params=(f"%{tag}%",)
     else:
         print("Invalid Option!")
         return
@@ -89,7 +90,6 @@ def filter_notes():
     if not results:
         print("NO Match Found")
         return
-    print(f"\nFound {len(results)} matching note(s):\n")
     for note in results:
         print('-'*40)
         print(f"ID: {note[0]}")
@@ -99,4 +99,83 @@ def filter_notes():
         print(f"Tags: {note[4]}")
         print(f"Created at: {note[5]}")
         print('-'*40)
-filter_notes()
+
+def update_note():
+    id=input("Enter the ID of the note to update: ").strip()
+    conn=sqlite3.connect("vault.db")
+    cursor=conn.cursor()
+    cursor.execute("SELECT * FROM notes WHERE id = ?", (id,))
+    note = cursor.fetchone()
+    if not note:
+        print("Note not found.")
+        conn.close()
+        return
+    print("\nLeave a field empty if you don't want to change it.\n")
+    new_title = input(f"New Title [{note[1]}]: ").strip() or note[1]
+    new_content = input(f"New Content [{note[2]}]: ").strip() or note[2]
+    new_category = input(f"New Category [{note[3]}]: ").strip() or note[3]
+    new_tags = input(f"New Tags [{note[4]}]: ").strip() or note[4]
+
+    cursor.execute("""
+        UPDATE notes 
+        SET title = ?, content = ?, category = ?, tags = ?
+        WHERE id = ?
+    """, (new_title, new_content, new_category, new_tags, id))
+    conn.commit()
+    conn.close()
+    print("Note Updated Successfully!")
+
+def delete_note():
+    delete_id=input("Enter Id of the Note to delete: ")
+    conn=sqlite3.connect("vault.db")
+    cursor=conn.cursor()
+    cursor.execute("Select * from notes where id=?",(delete_id,))
+    note = cursor.fetchone()
+    if not note:
+        print("Note not found.")
+        conn.close()
+        return
+    confirm=input(f"Are you sure you want to delete note '{note[1]}'? (y/n): ").lower()
+    if confirm == 'y':
+        cursor.execute("DELETE FROM notes WHERE id = ?", (delete_id,))
+        conn.commit()
+        print("Note deleted successfully!")
+    else:
+        print("Deletion cancelled.")
+
+    conn.close()
+
+def main():
+    while True:
+        print("\n📚 PERSONAL KNOWLEDGE VAULT")
+        print("1. Add a new note")
+        print("2. View all notes")
+        print("3. Search notes by keyword")
+        print("4. Filter notes by category or tag")
+        print("5. Update a note")
+        print("6. Delete a note")
+        print("7. Exit")
+
+        choice = input("Enter your choice (1-7): ").strip()
+
+        if choice == '1':
+            add_note()
+        elif choice == '2':
+            view_notes()
+        elif choice == '3':
+            search_note()
+        elif choice == '4':
+            filter_notes()
+        elif choice == '5':
+            update_note()
+        elif choice == '6':
+            delete_note()
+        elif choice == '7':
+            print("Exiting... Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
+
